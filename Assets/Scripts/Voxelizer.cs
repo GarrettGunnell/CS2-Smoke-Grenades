6a1ffc55e00adb2000cbfedfcf10b3ba69b51116
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-04-15 17:17:32 -0700

    Add script for voxelizing the scene

--- Assets/Scripts/Voxelizer.cs
+++ Assets/Scripts/Voxelizer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VolumeNoiseGenerator : MonoBehaviour
+public class Voxelizer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
